847afa0ebc17744e7a163920cf3bc0a0681f7344
Felipe Manchester <f.p2809@gmail.com>
2023-04-14 22:08:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function dropFileEffect() {
+  const dropArea = document.querySelector(".drag-files");
+
+  dropArea.addEventListener("dragover", () => {
+    dropArea.classList.add("dragover");
+  });
+
+  dropArea.addEventListener("dragleave", () => {
+    dropArea.classList.remove("dragover");
+  });
+}
+
+dropFile();
